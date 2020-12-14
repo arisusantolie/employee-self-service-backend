c5b86ad164060173d41ff34f224b47693b2634e6
arisusantolie <arisusantolie@gmail.com>
2020-12-14 13:00:10 +0700

    update

--- src/main/java/com/project/ess/EssApplication.java
+++ src/main/java/com/project/ess/EssApplication.java
@@ -1,9 +1,13 @@
 package com.project.ess;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 
 @SpringBootApplication
 public class EssApplication {
